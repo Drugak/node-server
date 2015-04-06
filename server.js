068ca0a9cfe78789f5c4765cb0e35ae5517cbe94
Vitaliu Drugak <vitosdrugak92@gmail.com>
2015-04-06 17:07:22 +0300

    module "fs"

--- server.js
+++ server.js
@@ -4,17 +4,8 @@
 
 "use strict"
 
-var http =require('http');
+var fs = require('fs');
 
-var server = new http.Server(function(req, res) {}).listen(3000);
-
-setTimeout(function(){
-    server.close();
-}, 2500);
-
-
-var timer = setInterval(function(){
-    console.log(process.memoryUsage());
-}, 1000);
-
-timer.unref();
+fs.readFile("dasdasdads", {encoding: 'utf-8'}, function(err, data){
+    err ? console.log(err) : console.log(data);
+});
